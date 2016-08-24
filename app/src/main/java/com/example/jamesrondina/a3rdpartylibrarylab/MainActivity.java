c0d789d7546b4996b69a16bbac42f714ee64ca14
James <jamesrondina@gmail.com>
2016-08-24 11:15:16 -0400

    jrondina put a little bit more effort

--- app/src/main/java/com/example/jamesrondina/a3rdpartylibrarylab/MainActivity.java
+++ app/src/main/java/com/example/jamesrondina/a3rdpartylibrarylab/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jamesrondina.a3rdpartylibrarylab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +17,9 @@ import com.android.volley.toolbox.Volley;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    Button benjamin;
+
     private String myUrl = "https://www.chess.com/";
 
 
@@ -23,27 +29,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        benjamin = (Button) findViewById(R.id.benjamin);
 
-        final StringRequest stringRequest = new StringRequest(Request.Method.GET, myUrl,
-                new Response.Listener<String>() {
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                myUrl = editText.getText().toString();
+
+                final StringRequest stringRequest = new StringRequest(Request.Method.GET, myUrl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                textView.setText(response);
+
+
+                            }
+                        }, new Response.ErrorListener() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onErrorResponse(VolleyError error) {
 
-                        textView.setText(response);
-
+                        Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
 
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+                });
 
-                Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_SHORT).show();
-                error.printStackTrace();
+                Volley.newRequestQueue(MainActivity.this).add(stringRequest);
 
             }
-        });
+        };
 
-        Volley.newRequestQueue(this).add(stringRequest);
+        benjamin.setOnClickListener(listener);
+
+
 
     }
 }
